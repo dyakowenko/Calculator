1001a576d0099f0411f6bebd48cb9e41be4bc061
vnikitin <valik071111@gmail.com>
2020-04-14 13:48:59 +0300

    added template

---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Calculator/SharedResources.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace Calculator
+{
+    public static class SharedResources
+    {
+        public static Color OpButtonBkColor
+        {
+            get
+            {
+                //return Color.White;
+                return Color.FromRgb(0xff, 0xa5, 0);
+            }
+        }
+    }
+}
